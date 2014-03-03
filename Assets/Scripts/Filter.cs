b312782b1d16d342939537e489b95f6ee98f5699
Fredhhh <fred.stangervag@hotmail.com>
2014-03-03 21:40:30 +0100

    ADDED EVRITING

---
+++ Assets/Scripts/Filter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Filter : MonoBehaviour
+
+
+{
+	
+	public float timeLeft;
+
+	public AudioClip audio1;
+	public AudioClip audio2;
+
+	private gamestate state;
+
+	void Start() {
+		state = gamestate.Instance;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+
+		if (state.normalPerception) {
+			this.gameObject.renderer.enabled = false;
+		} else {
+			this.gameObject.renderer.enabled = true;
+		}
+
+	}
+	
+}
+
---
+++ Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPhysics : MonoBehaviour {
+
+	//[HideInInspector]
+	//public bool grounded;
+
+		public void Move(Vector2 moveAmount) {
+			float deltaY = moveAmount.y;
+			float deltaX = moveAmount.x;
+		//Vector2 p = transform.position;
+			//transform.Translate(moveAmount);
+		Vector2 finalTransform = new Vector2(deltaX,deltaY);
+		
+		transform.Translate(finalTransform);
+		}
+	
+
+}

3ea604af131aae2ae6fc17db39480283cbf037e5
Fredhhh <fred.stangervag@hotmail.com>
2014-03-05 20:11:04 +0100

    Create Egg

---
+++ Assets/Scripts/CreateEgg.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateEgg : MonoBehaviour {
+
+	public GameObject eggPrefab;
+	public GameObject egg;
+
+	// Use this for initialization
+	void Start () {
+		eggPrefab = GameObject.FindGameObjectWithTag("Egg");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			egg = Instantiate(eggPrefab, transform.FindGameObjectWithTag("eggSpawn").transform.position, Quaternion.identity);
+			egg.tag = "Egg";
+		}
+
+	}
+}

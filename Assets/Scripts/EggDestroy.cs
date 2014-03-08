dbc27e6d305854c87f1e2b1bc06793aabe38aa46
Fredhhh <fred.stangervag@hotmail.com>
2014-03-08 23:20:31 +0100

    Create Egg is crazy

---
+++ Assets/Scripts/EggDestroy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EggDestroy : MonoBehaviour {
+
+	public float timer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer+=Time.deltaTime;
+
+	
+	}
+
+	public float getTime() {
+
+		return timer;
+
+		}
+}

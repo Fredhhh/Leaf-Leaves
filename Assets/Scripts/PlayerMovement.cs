3ab45b57cd3dc43b51cac5345e43dca773b7b770
choisant <aurora.grefsrud@gamil.com>
2014-03-10 21:24:59 +0100

    Fixed jumping issue

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour {
 	public float inSpeed = 4.0f;
 	public bool facingRight;
 	public bool isGrounded;
+	public float jumpHeight = 300;
 
 
 	// Update is called once per frame
@@ -14,45 +15,44 @@ public class PlayerMovement : MonoBehaviour {
 
 	}
 	void Update () {
-		if(Input.GetKey(KeyCode.D)){
-			transform.Translate(Vector3.right * speed * Time.deltaTime);
 
-			if(facingRight==false)
-				{
-					facingRight=true;
+				if (Input.GetKey (KeyCode.D)) {
+						transform.Translate (Vector3.right * speed * Time.deltaTime);
+
+						if (facingRight == false) {
+								facingRight = true;
 				
-					gameObject.transform.localScale = new Vector3(1, 1, 1);
+								gameObject.transform.localScale = new Vector3 (1, 1, 1);
 				
-				}
-			}
-
-		if(Input.GetKey(KeyCode.A)){
-			transform.Translate(Vector3.left * speed * Time.deltaTime);
-
-
-				if(facingRight==true)
-				{
-					facingRight=false;
-			
-					gameObject.transform.localScale = new Vector3(-1, 1, 1);                             
-					
+						}
 				}
 
-			}
+				if (Input.GetKey (KeyCode.A)) {
+						transform.Translate (Vector3.left * speed * Time.deltaTime);
 
 
-		//if(Input.GetKey(KeyCode.S))
-			//transform.Translate(new Vector3(0, 1, 0) * inSpeed * Time.deltaTime);
-
-		if(Input.GetKey(KeyCode.Space)&& isGrounded==true){
-			this.gameObject.rigidbody2D.AddForce(Vector3.up * 260);
+						if (facingRight == true) {
+								facingRight = false;
 			
-			isGrounded = false;
+								gameObject.transform.localScale = new Vector3 (-1, 1, 1);                             
+						}
+
+				}
+				if (Input.GetKey (KeyCode.A) && Input.GetKey (KeyCode.D)) {
+				} 
+				else {
+						if (Input.GetKey (KeyCode.Space) && isGrounded) {
+								//transform.Translate (Vector3.up * jumpheight * speed * Time.deltaTime);
+								this.gameObject.rigidbody2D.AddForce (Vector3.up * jumpHeight);
+								isGrounded = false;
+						}
+				}
+				
+
 		}
-		Debug.Log (isGrounded);
+		//Debug.Log (isGrounded);
 		
 
-	}
 
 	void OnCollisionEnter2D (Collision2D c)
 	{

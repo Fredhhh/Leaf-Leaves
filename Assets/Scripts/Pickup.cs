3cee35953fa81ca579b3639b9fb9534a01d7f621
Fredhhh <fred.stangervag@hotmail.com>
2014-03-12 15:39:25 +0100

    PickUpItem

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+	public GameObject pickableItems;
+	public int currentItems;
+
+	// Use this for initialization
+	void Start () {
+
+		currentItems = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (currentItems == 3)
+		{
+
+		}
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D c)
+	{
+		GameObject Fruit = GameObject.FindGameObjectWithTag("Fruit");
+
+		if (c.tag.Equals("Fruit"))
+		{
+			currentItems = currentItems + 1;
+			Destroy (Fruit);
+		}
+	}
+
+
+}

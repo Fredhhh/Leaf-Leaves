ff6b2ff97554b89a52191bf5841564b6a66f659e
Fredhhh <fred.stangervag@hotmail.com>
2014-05-05 20:28:27 +0200

    Revert "PLAYERMOVEMENT BYEBYE"
    This reverts commit a581172cec63ef53c6a8340acb23b85b36375f29.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+//[RequireComponent(typeof(GroundedScript))]
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 4.0f;
+	public float inSpeed = 4.0f;
+	public bool facingRight;
+	public float jumpHeight = 300;
+	public float ray1 = 1f;
+	public bool isGrounded;
+	//public bool isGrounded;
+	
+	
+	
+	
+	// Update is called once per frame
+	void Start (){
+		facingRight = true;
+		isGrounded = false;
+		
+		
+	}
+	void Update () {
+		
+		
+		
+		if (Input.GetKey (KeyCode.D)) {
+			transform.eulerAngles = new Vector3(0, 0, 0);
+			transform.Translate (Vector3.right * speed * Time.deltaTime);
+			
+			/*if (facingRight == false) {
+				facingRight = true;
+				
+				gameObject.transform.localScale = new Vector3 (1, 1, 1);
+				
+			}*/
+		}
+		
+		if (Input.GetKey (KeyCode.A)) {
+			transform.eulerAngles = new Vector3(0, 180, 0);
+			transform.Translate (Vector3.right * speed * Time.deltaTime);
+			
+			
+			/*if (facingRight == true) {
+				facingRight = false;
+				
+				gameObject.transform.localScale = new Vector3 (-1, 1, 1);                             
+			}*/
+			
+		}
+		
+		
+		if (Input.GetButtonDown("Jump") && isGrounded) {
+			//transform.Translate (Vector3.up * jumpheight * speed * Time.deltaTime);
+			this.gameObject.rigidbody2D.AddForce (Vector3.up * jumpHeight);
+			//isGrounded = false;
+			
+		}
+		
+	}
+	
+	void OnTriggerEnter2D (Collider2D c)
+	{
+		if (c.gameObject.tag.Equals ("Ground")||c.gameObject.tag.Equals ("Egg")||c.gameObject.tag.Equals ("Hen"))
+		{
+			isGrounded = true;
+		}
+		
+		else
+			
+			isGrounded = false;
+		
+	}
+	void OnTriggerExit2D (Collider2D u)
+	{
+		if (u.gameObject.tag.Equals ("Ground")||u.gameObject.tag.Equals ("Egg")||u.gameObject.tag.Equals("Hen"))
+		{
+			isGrounded = false;
+		}
+
+
+		if (u.gameObject.tag.Equals("Basket"))
+		{
+			isGrounded = true;
+		}
+	}
+	
+	
+	
+	/*void OnCollisionEnter2D (Collision2D c)
+	{
+
+	}*/
+}

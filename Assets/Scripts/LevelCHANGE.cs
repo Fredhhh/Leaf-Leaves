303905591ba4b189f55a67966aa6f62d957ef510
Fredhhh <fred.stangervag@hotmail.com>
2014-05-05 23:12:14 +0200

    Load Levels!

---
+++ Assets/Scripts/LevelCHANGE.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelCHANGE : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D c)
+	{
+		if (c.gameObject.name.Equals("LoadLVL1"))
+		{
+			Application.LoadLevel("Beta1");
+		}
+
+		if (c.gameObject.name.Equals("sopp"))
+		{
+			if (GetComponent<Pickup>().currentHens == 4)
+			{
+				Application.LoadLevel("Choose_level");
+			}
+
+			else 
+				Debug.Log("You need to catch all of the stupid turkeys!");
+		}
+	}
+}

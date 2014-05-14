808930338e77b8cbf24773547f5bccf891c26f82
Fredhhh <fred.stangervag@hotmail.com>
2014-05-14 13:36:10 +0200

    FERDIG??? V.1.0

--- Assets/Scripts/LevelCHANGE.cs
+++ Assets/Scripts/LevelCHANGE.cs
@@ -11,8 +11,7 @@ public class LevelCHANGE : MonoBehaviour {
 	public GameObject Laks;
 
 	public GameObject Egg;
-
-	// Use this for initialization
+	
 	void Start () {
 
 		chooseLevel = false;
@@ -20,19 +19,21 @@ public class LevelCHANGE : MonoBehaviour {
 
 	}
 	
-	// Update is called once per frame
+	// Update blir kalt per frame
 	void Update () {
 
 		Marsvin = GameObject.FindGameObjectWithTag("marsvin");
 		Sopp = GameObject.FindGameObjectWithTag("sopp");
 		Laks = GameObject.FindGameObjectWithTag("laks");
 
+		//hvis man har fått fire kalkuner og levelet er nr. 0 skal soppen blokkere inngangen til level 1, men marsvinet fjerner sin blokkade
 		if (GetComponent<Pickup>().currentHens == 4 && lvl == 0)
 		{
 			Marsvin.collider2D.enabled = false;
 			Sopp.collider2D.enabled = true;
 		}
 
+		//hvis man har fire kalkuner, levelet er nr. 0, og man har fått riktig frukt i level 2, skal marsvinet blokkere level 2, og laksen åpne sin
 		if (GetComponent<Pickup>().fruitIsCorrect == true && GetComponent<Pickup>().currentHens == 4 && lvl == 0)
 		{
 			Laks.collider2D.enabled = false;
@@ -40,6 +41,7 @@ public class LevelCHANGE : MonoBehaviour {
 		}
 	}
 
+	//funksjonen Awake unngår å fjerne objektet skriptet er knyttet til, dvs. karakteren 
 	void Awake ()
 	{
 		DontDestroyOnLoad(gameObject);
@@ -48,21 +50,28 @@ public class LevelCHANGE : MonoBehaviour {
 	void OnLevelWasLoaded (int level){
 		lvl = level;
 
+		GetComponent<CreateEgg>().currentTime = 0;
+		GetComponent<CreateEgg>().compareTime = 0;
+
+		//hvor hvert nye innlastede level settes antallet egg lagt ut til 0
 		if (lvl == 0)
 		{
 			GetComponent<CreateEgg>().eggCount = 0;
 		}
 
+		//for noen level blir karakterens startposisjon endret ved bruk av new Vector3
 		if (lvl == 1)
 		{
-			gameObject.transform.position = new Vector3 (6 , 1 , 0);
+			gameObject.transform.position = new Vector3 (5 , 3 , 0);
+
+			Egg.renderer.sortingOrder = 2;
 
 			GetComponent<CreateEgg>().eggCount = 0;
 		}
 
 		if (lvl == 2)
 		{
-			gameObject.transform.position = new Vector3 (5 , 1 , 11);
+			gameObject.transform.position = new Vector3 (4 , 3 , 11);
 			gameObject.renderer.sortingOrder = 1;
 
 			Egg.renderer.sortingOrder = 0;
@@ -72,10 +81,10 @@ public class LevelCHANGE : MonoBehaviour {
 
 		if (lvl == 3)
 		{
-			gameObject.transform.position = new Vector3 (4 , 1 , 10);
-			gameObject.renderer.sortingOrder = -3;
+			gameObject.transform.position = new Vector3 (3 , 3 , 10);
+			gameObject.renderer.sortingOrder = 3;
 			
-			Egg.renderer.sortingOrder = -4;
+			Egg.renderer.sortingOrder = 3;
 			
 			GetComponent<CreateEgg>().eggCount = 0;
 		}
@@ -83,7 +92,7 @@ public class LevelCHANGE : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D c)
 	{
-
+		//denne funksjonen laster inn et nytt level når karakteren kolliderer med en bestemt kollisjonsboks 
 		if (lvl == 0)
 		{
 			if (c.gameObject.name.Equals("LoadLVL1"))
@@ -100,29 +109,37 @@ public class LevelCHANGE : MonoBehaviour {
 			{
 				Application.LoadLevel("Escape");
 			}
+
 		}
 
 		if (c.gameObject.tag.Equals("sopp"))
 		{
+		 
 			if (lvl == 1)
 			{
+				//er levelet 1 og man har alle fire kalkunene vil man komme tilbake til valg av level-banen
 				if (GetComponent<Pickup>().currentHens == 4)
 				{
 
 					Application.LoadLevel("Choose_level");
 					chooseLevel = true;
 				}
+
 			}
+
 		}
 
 
 		if (c.gameObject.tag.Equals("marsvin"))
 		{
-
-			if (GetComponent<Pickup>().fruitIsCorrect == true && lvl == 2)
+			if (lvl == 2)
 			{
-				Application.LoadLevel("Choose_level");
-				chooseLevel = true;
+				//er levelet 2 og man har riktig frukt vil man komme tilbake til valg av level-banen
+				if (GetComponent<Pickup>().fruitIsCorrect == true)
+				{
+					Application.LoadLevel("Choose_level");
+					chooseLevel = true;
+				}
 			}
 
 		}
--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -5,23 +5,15 @@ public class Pickup : MonoBehaviour {
 
 	public GameObject spawnPoint;
 	public GameObject[] heldItems;
-	//public GameObject[] heldHens;
 	public int currentItems;
 	public int currentHens;
 	public int lvl;
-
-	//public int i;
-	//public int d;
 
 	public bool canPickup;
 	public bool fruitIsCorrect;
 
 	// Use this for initialization
 	void Start () {
-
-		lvl = GetComponent<LevelCHANGE>().lvl;
-
-		//i = 0;
 
 		currentItems = 0;
 		currentHens = 0;
@@ -36,7 +28,7 @@ public class Pickup : MonoBehaviour {
 	
 	}
 	
-	// Update is called once per frame
+	// i Update sjekker vi om karakteren har plukket opp for mye eller for lite frukt for å definere når man kan plukke opp mer
 	void Update () {
 
 
@@ -50,28 +42,33 @@ public class Pickup : MonoBehaviour {
 			canPickup = true;
 		}
 	}
-	
+
+	void OnLevelWasLoaded (int level){
+		lvl = level;
+	}
 
 	void OnTriggerEnter2D (Collider2D c)
 	{
 
 				if (canPickup) {
 
+						//i denne loopen sjekker vi hver frukt som karakteren plukker opp
 						foreach (GameObject Fruit in GameObject.FindGameObjectsWithTag("Fruit")) {
 
+								//for hvert objekt karakteren er borti som er en frukt, rendres(vises) ikke den frukten, og antallet objekter karakteren har - stiger
 								if (c.gameObject == Fruit) {
 										heldItems [currentItems] = Fruit.gameObject; 
 										Fruit.gameObject.renderer.enabled = false;
 										Fruit.gameObject.collider2D.enabled = false;
 
 										currentItems++;
-										//i++;
 
 								}
 						}
-
+						//i denne loopen sjekker vi hver kalkun som karakteren plukker opp
 						foreach (GameObject Hen in GameObject.FindGameObjectsWithTag("Hen")) {
 							
+							//for hvert objekt karakteren er borti som er en kalkun, forsvinner kalkunen og antallet kalkuner man har plukket opp stiger
 							if (c.gameObject == Hen) {
 							
 								Destroy (Hen);
@@ -81,11 +78,29 @@ public class Pickup : MonoBehaviour {
 							}
 					}
 				} 
+		if (lvl == 0)
+		{
+			if (c.tag.Equals ("sopp"))
+			{
+				Debug.Log("Hello there, Leaf! Jump with space and lay your eggs with shift when air-born to catch my turkeys!");
+			}
+
+			if (c.tag.Equals ("marsvin"))
+			{
+				Debug.Log("Oh! Hurry, we need to get some fruit!");
+			}
+
+			if (c.tag.Equals ("laks"))
+			{
+				Debug.Log ("This is the final trial, please, Leaf, leave the forest!");
+			}
+		}
+
 
 		if (lvl == 1)
 		{
 
-		if (c.name.Equals ("Sopp"))
+		if (c.name.Equals ("sopp"))
 			{
 
 				if (currentHens < 4)
@@ -99,31 +114,34 @@ public class Pickup : MonoBehaviour {
 			if (lvl == 2)
 			{
 				if (c.tag.Equals ("marsvin")) {
+
 						if (currentItems == 3) 
 								{
+								//hvis karakteren har fylt opp fruktarrayet heldItems, og navnene stemmer i koden og i spillet skal man få beskjed om at det er riktig frukt
 								if (heldItems [0].name.Equals ("cherry") && heldItems [1].name.Equals ("plum") && heldItems [2].name.Equals ("melon")) {
 									Debug.Log ("These are the right fruits!");
 									fruitIsCorrect = true;
 								} 
 									
-									foreach (GameObject Fruit in GameObject.FindGameObjectsWithTag("Fruit"))
-									{
-										
-										Fruit.gameObject.renderer.enabled = true;
-										Fruit.gameObject.collider2D.enabled = true;
-										
-										if (fruitIsCorrect == false && currentItems == 3)
-											{
-												Debug.Log ("No, that's wrong, I said cherry, plum and melon!");
-											}
+								//hver gang karakteren er borti marsvinets kollisjonsdetektor og har tre frukter, vil fruktene automatisk dukke opp igjen på trærne,
+								//slik at karakteren kan hente de riktige om frukten var feil
+								foreach (GameObject Fruit in GameObject.FindGameObjectsWithTag("Fruit"))
+								{
+									Fruit.gameObject.renderer.enabled = true;
+									Fruit.gameObject.collider2D.enabled = true;
+									
+									//hvis frukten er feil når karakteren er borte ved marsvinet blir fruktinformasjonen nullstilt, og karakteren kan fylle opp igjen fruktarrayet
+									if (fruitIsCorrect == false && currentItems == 3)
+										{
+											currentItems = 0;
+											
+											heldItems = new GameObject[3]{null, null, null};
 
-										currentItems = 0;
-										//i = 0;
-										heldItems = new GameObject[3]{null, null, null};
-										
-										//GameObject F = (GameObject)Instantiate(Fruit, spawnPoint.transform.position, Quaternion.identity);
-										//F.transform.position = spawnPoint.transform.position;
-									}
+											Debug.Log ("No, that's wrong, I said cherry, plum and melon!");
+										}
+
+				
+								}
 
 				
 									if (fruitIsCorrect == false && currentItems > 0)
@@ -134,10 +152,15 @@ public class Pickup : MonoBehaviour {
 								}
 						else
 						{
-							Debug.Log ("Please pick three fruits. A cherry, a plum and a melon!");
+							Debug.Log ("You can only pick three fruits at the time. A cherry, a plum and a melon, and in the right order!");
 						}
 					}
 				}
+
+			if (c.gameObject.name.Equals("WinPeople"))
+		    {
+			Debug.Log("Leaf left!!");
+			}
 					
 		}
 	
